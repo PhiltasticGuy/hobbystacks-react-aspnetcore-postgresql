b2d74b87d0bffa7fbab05e1b0a82b66a13acd9a1
Philippe Turcotte <pturcotte@xorcube.com>
2019-09-15 04:44:56 +0000

    Merged PR 16: Fixed paths to deployment files in deploy.sh.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,8 +11,8 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 logfile="deploy.log"
 
 # Rename deployment files.
-mv nginx.deploy.conf ../data/nginx/nginx.conf
-mv docker-compose.deploy.yml ../docker-compose.yml
+mv hobbystacks/deploy/nginx.deploy.conf hobbystacks/data/nginx/nginx.conf
+mv hobbystacks/deploy/docker-compose.deploy.yml hobbystacks/docker-compose.yml
 
 docker-compose down &>> $logfile
 docker login -u $1 -p $2 xorcube.azurecr.io &>> $logfile
